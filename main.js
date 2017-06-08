15e1e58538dc666f48efd34b6c98a5ff796fa61b
jzmstrjp <jazzm@ster.jp.net>
2017-06-08 20:31:40 +0900

    directory
    directory

--- main.js
+++ main.js
@@ -7,62 +7,35 @@ define(function(require, exports, module) {
 		EditorManager = brackets.getModule("editor/EditorManager"),
 		KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
 		CommandManager = brackets.getModule("command/CommandManager"),
-		KeyEvent = brackets.getModule("utils/KeyEvent"),
 		AppInit = brackets.getModule("utils/AppInit"),
 		ProjectManager = brackets.getModule("project/ProjectManager"),
 		FileSystem = brackets.getModule("filesystem/FileSystem"),
 		FileUtils = brackets.getModule("file/FileUtils"),
 		ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
-	//var modalHTML = require("text!modal.html");
-
 	var DRAG_AND_MOVE = require("drag_and_move");
+
+	var dropZone;
 
 	var commandID_modal = "jzmstrjp.drop_includer.drop_includer_open",
 		commandID_browse_root_path = "jzmstrjp.drop_includer.drop_includer_browse_root_path",
 		commandID_browse_relative_path = "jzmstrjp.drop_includer.drop_includer_browse_relative_path";
 
-
 	var currentDoc,
 		editor;
 
-	var dropZone;
-
-
-
+	var cssLikeModeRegExp = /^(text\/x-less|text\/x-scss|css)$/;
 
 	ExtensionUtils.loadStyleSheet(module, "main.less");
 
 	CommandManager.register("Open Drop Includer", commandID_modal, openDialog);
 	KeyBindingManager.addBinding(commandID_modal, "Ctrl-.");
 
-	CommandManager.register("Drop Includer(Browse: Root Path)", commandID_browse_root_path, function() {
-		openBrowse({ root: true });
-	});
+	CommandManager.register("Drop Includer(Browse: Root Path)", commandID_browse_root_path, function() { openBrowse({ root: true }); });
 	KeyBindingManager.addBinding(commandID_browse_root_path, "Ctrl-Shift-.");
 
 	CommandManager.register("Drop Includer(Browse: Relative Path)", commandID_browse_relative_path, openBrowse);
 	KeyBindingManager.addBinding(commandID_browse_relative_path, "Ctrl-Shift-Alt-.");
-
-
-	//テスト用
-	CommandManager.register("テスト1", "jzmstrjp.drop_includer.test1", test_function1);
-	//KeyBindingManager.addBinding("jzmstrjp.drop_includer.test1", "F9");
-	CommandManager.register("テスト2", "jzmstrjp.drop_includer.test2", test_function2);
-	//KeyBindingManager.addBinding("jzmstrjp.drop_includer.test2", "F10");
-	var test_hensu1;
-
-	function test_function1() {
-		var editor = EditorManager.getCurrentFullEditor();
-		alert(editor.getModeForSelection());
-		test_hensu1 = editor.getSelections();
-	}
-
-	function test_function2() {
-		var editor = EditorManager.getCurrentFullEditor();
-		editor.setSelections(test_hensu1);
-	}
-
 
 	function openBrowse(obj) {
 		var root = false;
@@ -71,7 +44,6 @@ define(function(require, exports, module) {
 			root = true;
 			addTitle = "(Root Path)";
 		}
-
 
 		currentDoc = DocumentManager.getCurrentDocument();
 		if (!currentDoc) {
@@ -91,49 +63,39 @@ define(function(require, exports, module) {
 	}
 
 
-	function forEachRun(editor, docPath, paths, root) {
-		//paths.push(paths.shift());
-		//paths.sort();
+	function isDir(path){
+		var rtn;
+		FileSystem.resolve(path, function(errorString, fileSystemEntry, fileSystemStats){
+			rtn = fileSystemEntry._isDirectory;
+		});
+		return rtn;
+	}
 
+	function forEachRun(editor, docPath, paths, root) {
+		//alert(getMode(editor));
 		var selections = editor.getSelections();
 		var multiBGI = false;
 
-		if (paths.length > 1) {
-			var imgExArr = ["jpg", "jpeg", "png", "gif", "svg"]; //画像拡張子一覧
-			var boolArr = []; //画像かどうかを格納していく配列
-			var all_img = false;
+		if (paths.length > 1) { //ファイルが1個なら複数背景の必要なし。
+			multiBGI = true; //全部画像かどうか。デフォtrue。
 
 			paths.forEach(function(path) {
-				if (imgExArr.indexOf(FileUtils.getFileExtension(path)) === -1) {
-					boolArr.push(false);
-				} else {
-					boolArr.push(true);
+				if (!/^(jpg|jpeg|png|gif|svg)$/.test(FileUtils.getFileExtension(path)) || isDir(path)) {
+					multiBGI = false; //1つでも画像拡張子じゃなければfalseに。または1つでもディレクトリならfalseに。
 				}
 			});
-			if (boolArr.length > 0 && boolArr.indexOf(false) === -1) { //全部true（画像）だったら
-				//alert("全部画像");
-				all_img = true;
-			}
-			if (all_img) {
-				switch (getMode(editor)) {
-					case "text/x-less":
-					case "text/x-scss":
-					case "css":
-						//alert("全部画像だしcss上に貼り付け用としている");
-						multiBGI = true;
-						break;
-				}
+
+			if (!cssLikeModeRegExp.test(getMode(editor))) {
+				multiBGI = false; //modeがcss系じゃなければfalseに。
 			}
 		}
-
-
 
 		currentDoc.batchOperation(function() {
 			if (paths.length > 1 && selections.length === paths.length) {
 				var relativeFilenameArr = [];
 				paths.forEach(function(elm) {
 					var relativeFilename = abspath2rel(docPath, elm, root);
-					relativeFilename = tagMaker(relativeFilename, root, editor);
+					relativeFilename = tagMaker(relativeFilename, root, editor, isDir(elm));
 					relativeFilenameArr.push(relativeFilename);
 				});
 				one_by_one(relativeFilenameArr, selections);
@@ -152,11 +114,10 @@ define(function(require, exports, module) {
 				editor.getSelections().forEach(function(sel, i, array) {
 					editor.document.replaceRange("\n", editor.getSelections()[i]["start"]);
 				});
-
 			} else {
 				paths.forEach(function(elm) {
 					var relativeFilename = abspath2rel(docPath, elm, root);
-					relativeFilename = tagMaker(relativeFilename, root, editor);
+					relativeFilename = tagMaker(relativeFilename, root, editor, isDir(elm));
 
 					doInsert({ text: relativeFilename });
 					if (paths.length === 1 && relativeFilename.slice(0, 4) === "<img") {
@@ -173,19 +134,6 @@ define(function(require, exports, module) {
 		});
 	}
 
-
-
-	/*****************************
-	 * init
-	 */
-	function init() {
-		initDropDialog();
-	}
-
-
-	/*****************************
-	 * drop dialog initialize
-	 */
 	function initDropDialog() {
 		dropZone = document.createElement("div");
 		dropZone.id = "tagInserterDropZone";
@@ -211,9 +159,6 @@ define(function(require, exports, module) {
 	}
 
 
-	/******************************
-	 * drag and drop handle
-	 */
 	function _handleDrop(e) {
 		var root = false;
 		if (this.id === "tagInserterRoot") {
@@ -246,66 +191,59 @@ define(function(require, exports, module) {
 		}
 	}
 
-
-
-
 	function getMode(editor) {
 		var mode = editor.getModeForSelection();
 		return mode;
 	}
 
-	function tagMaker(path, root, editor) {
+	function tagMaker(path, root, editor, dir) {
+		if(dir){
+			return path + "/";
+		}
+		var rtn;
 		var mode = getMode(editor);
+		var ex = FileUtils.getFileExtension(path);
+		var noEx = FileUtils.getFilenameWithoutExtension(path);
+		var tags = {
+			js: "script",
+			mp3: "audio",
+			wav: "audio",
+			mp4: "video"
+		};
 		var phpStart = '<?php ';
 		var phpEnd = '?>';
 		if (mode === "clike") {
 			phpStart = '';
 			phpEnd = '';
 		}
-		var rtn;
-		var ex = FileUtils.getFileExtension(path);
-		var noEx = FileUtils.getFilenameWithoutExtension(path);
 		switch (ex) {
 			case "jpg":
 			case "jpeg":
 			case "png":
 			case "gif":
 			case "svg":
-				switch (mode) {
-					case "text/x-less":
-					case "text/x-scss":
-					case "css":
-						rtn = 'background-image: url(' + path + ');';
-						break;
-					default:
-						rtn = '<img src="' + path + '" alt="xxxxx">';
+				if (cssLikeModeRegExp.test(mode)) { //modeがcss系なら
+					rtn = 'background-image: url(' + path + ');';
+				} else {
+					rtn = '<img src="' + path + '" alt="xxxxx">';
 				}
 				break;
 			case "css":
 			case "scss":
 			case "less":
-				switch (mode) {
-					case "text/x-" + ex:
-						rtn = '@import "' + path + '";';
-						break;
-					case "text/x-scss":
-					case "text/x-less":
-					case "css":
-						rtn = '@import "' + noEx + '.css";';
-						break;
-					default:
-						rtn = '<link rel="stylesheet" href="' + noEx + '.css">';
+				if(mode === "text/x-" + ex){ //scssをscssにimportするなら
+					rtn = '@import "' + path + '";';
+				} else if (cssLikeModeRegExp.test(mode)) { //modeがcss系なら
+					rtn = '@import "' + noEx + '.css";';
+				} else {
+					rtn = '<link rel="stylesheet" href="' + noEx + '.css">';
 				}
 				break;
 			case "js":
-				rtn = '<script src="' + path + '"></script>';
-				break;
 			case "mp3":
 			case "wav":
-				rtn = '<audio src="' + path + '"></audio>';
-				break;
 			case "mp4":
-				rtn = '<video src="' + path + '"></video>';
+				rtn = '<' + tags[ex] + ' src="' + path + '"></' + tags[ex] + '>';
 				break;
 			case "html":
 			case "php":
@@ -332,7 +270,6 @@ define(function(require, exports, module) {
 		e.preventDefault();
 	}
 
-
 	function abspath2rel(base_path, target_path, root) {
 		if (root) {
 			var projectRootPath = ProjectManager.getInitialProjectPath();
@@ -351,18 +288,13 @@ define(function(require, exports, module) {
 		for (var i = 0; i < base_path.length; i++) {
 			tmp_str += '../';
 		}
-		if (tmp_str === ''){
+		if (tmp_str === '') {
 			tmp_str = './';
 		}
 		return tmp_str + target_path.join('/');
 	}
 
-
-
-	/*****************************
-	 * insert
-	 */
-
+	/* insert */
 	function one_by_one(relativeFilenameArr, selections) {
 		var newSel;
 		selections.forEach(function(sel, i) {
@@ -371,35 +303,26 @@ define(function(require, exports, module) {
 		});
 	}
 
-
 	function doInsert(insertItem) {
 		var selections = editor.getSelections();
 		var edits = [];
 
 		selections.forEach(function(sel) {
 			queueEdits(edits, getEdits(sel, insertItem));
-			//editor.document.replaceRange(insertItem.text, sel.start, sel.end);
 		});
 
 		// perform edits
 		selections = editor.document.doMultipleEdits(edits);
-		//editor.setSelections(selections);
 
 		// indent lines with selections
 		selections.forEach(function(sel) {
 			if (!sel.end || sel.start.line === sel.end.line) {
-				// The document is the one that batches operations, but we want to use
-				// CodeMirror's indent operation. So we need to use the document's own
-				// backing editor's CodeMirror to do the indentation.
 				currentDoc._masterEditor._codeMirror.indentLine(sel.start.line);
 			}
 		});
 	}
 
-
-
 	function getEdits(sel, insertItem) {
-
 		var insertString = insertItem.text,
 			replSelEnd = $.extend({}, sel.end);
 
@@ -413,19 +336,13 @@ define(function(require, exports, module) {
 		};
 	}
 
-
 	function queueEdits(edits, val) {
 		if (val) {
 			edits.push(val);
 		}
 	}
 
-
-	// Initialize extension
 	AppInit.appReady(function() {
-		init();
+		initDropDialog();
 	});
-
-
-
 });
